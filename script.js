5c74eaa6de87baf9d5dca87a42cdf9e8e7c7ce60
Milos Milijasevic <milosrm20005@gmail.com>
2022-04-04 13:44:04 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict"
+
+
+const colorBox = document.querySelector(`.color-box`);
+const displayColor = document.querySelector(`.display-color`);
+const buttonNewColor = document.querySelector(`.new-color`);
+const buttonCopyText = document.querySelector(`.copy-color`);
+
+
