46ea861d592868926fbbe14263d728f6143ee2d9
Milos Milijasevic <milosrm20005@gmail.com>
2022-04-06 20:52:31 +0200

    updated

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ const buttonNewColor = document.querySelector(`.new-color`);
 const buttonCopyText = document.querySelector(`.copy-color`);
 
 
+const randomNumbers = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () => `rgb(${randomNumbers(0, 255)}, ${randomNumbers(0, 255)}, ${randomNumbers(0, 255)})`;
+
+buttonNewColor.addEventListener(`click`, () =>{
+    colorBox.style.backgroundColor = randomColor();
+    displayColor.value = randomColor();
+});
+
+buttonCopyText.addEventListener(`click`, () =>{
+    displayColor.select();
+    document.execCommand(`copy`);
+});
